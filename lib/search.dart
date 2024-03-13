449f4670bf102301d68998da510a0fe249719193
unknown <rojanparajuli13@gmail.com>
2024-03-13 20:21:43 +0545

    todaychanges

--- lib/search.dart
+++ lib/search.dart
@@ -10,33 +10,46 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
-     return  Scaffold(
+     return   Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-    Container(
-          child:Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextFormField(
-                controller: _SearchController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  labelText: 'Search',
-                  filled: true,
-                  prefixIcon: const Icon(Icons.search),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(color: Colors.blue),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
+        children: [
+        const  SizedBox(height: 20,),
+          Container(
+            padding: const  EdgeInsets.symmetric(horizontal: 10),
+            child: TextField(
+              controller: _SearchController,
+              decoration: InputDecoration(
+                hintText: ('Search'),
+                border:  OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.search_sharp, color: Colors.black),
+                  onPressed: () {},
                 ),
               ),
             ),
-
-      ),
-    ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Image.asset('assets/biryani.png'),
+              ),
+            ),
+          ),
+         const Text(' Special Biryan'),
+       const   SizedBox(height: 20,),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Expanded(
+              flex: 1,
+              child: Image.network('assets/KFC.png'),
+            ),
+          ),
+        ],
       ),
     );
   }
